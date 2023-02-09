c63740a539a2e180d1cac07150f1a0a9f91bd9d5
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-02-09 11:40:14 +0300

    fix dislikeRepository

--- src/main/java/com/kameleoon/userpost/persistence/PostDislikeRepository.java
+++ src/main/java/com/kameleoon/userpost/persistence/PostDislikeRepository.java
@@ -1,12 +1,11 @@
 package com.kameleoon.userpost.persistence;
 
 import com.kameleoon.userpost.entity.PostDislike;
-import com.kameleoon.userpost.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostDislikeRepository extends JpaRepository<PostDislike, Long> {
     boolean existsByUser_Login(String login);
-    PostLike findByUser_LoginAndPost_Id(String login, long id);
+    PostDislike findByUser_LoginAndPost_Id(String login, long id);
 }
